ffe04f135fbc8b0f3908c284b2c970bd484f8082
ShriRanjaniSR <136423361+ShriRanjaniSR@users.noreply.github.com>
2023-06-13 12:37:20 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -19,9 +19,9 @@ TEST_CASE("infers the breach according to limits") {
   REQUIRE(inferBreach(31, 10, 30) == TOO_HIGH);
 }
 
-//TEST_CASE("Classify breach based on cooling type") {
-//  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, -1) == TOO_LOW);
-//}
+TEST_CASE("Classify breach based on cooling type") {
+  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, -1) == TOO_LOW);
+}
           
 TEST_CASE("Classify breach based on cooling type") {
   REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, 10) == NORMAL);
@@ -31,21 +31,21 @@ TEST_CASE("Classify breach based on cooling type") {
   REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, 50) == TOO_HIGH);
 }
 
-//TEST_CASE("Classify breach based on cooling type") {
-//  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, -1) == TOO_LOW);
-//}
-//
-//TEST_CASE("Classify breach based on cooling type") {
-//  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 10) == NORMAL);
-//}
-//
-//TEST_CASE("Classify breach based on cooling type") {
-//  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 40) == TOO_HIGH);
-//}
+TEST_CASE("Classify breach based on cooling type") {
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, -1) == TOO_LOW);
+}
 
-//TEST_CASE("Classify breach based on cooling type") {
-//  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, -1) == TOO_LOW);
-//}
+TEST_CASE("Classify breach based on cooling type") {
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 10) == NORMAL);
+}
+
+TEST_CASE("Classify breach based on cooling type") {
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 40) == TOO_HIGH);
+}
+
+TEST_CASE("Classify breach based on cooling type") {
+  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, -1) == TOO_LOW);
+}
 
 TEST_CASE("Classify breach based on cooling type") {
   REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 10) == NORMAL);
