eb590af318ca793b1cf7d55829d58de30a21e81a
ShriRanjaniSR <136423361+ShriRanjaniSR@users.noreply.github.com>
2024-01-19 11:52:42 +0530

    Update StringLenthChecker.cpp

--- StringLenthChecker.cpp
+++ StringLenthChecker.cpp
@@ -3,18 +3,17 @@
 #include <vector>
 #include <string>
 
-int acceptStringVectorAndCheckForLength(const std::vector<std::string>& stringArray, int sizeLimit)
+void displayStringInput(const std::vector<std::string>& stringArray)
 {
-   if((stringArray.size() == 0) || sizeLimit < 0)
-   {
-       cout <<"Invalid Input \n";
-       return -1;
-   }
     cout << "The entered strings are: ";
     for(unsigned int i = 0; i < stringArray.size(); i++)
     {
 	cout << stringArray[i] << " ";
     }
+}
+
+int checkForSizeLimitAndPrintToConsole(const std::vector<std::string>& stringArray, int sizeLimit)
+{
     int count=0;
     cout << "The strings that qualify are: ";
     for(unsigned int i = 0; i < stringArray.size(); i++)
@@ -26,6 +25,35 @@ int acceptStringVectorAndCheckForLength(const std::vector<std::string>& stringAr
 	}
     }
     return count;
+}
+
+int acceptStringVectorAndCheckForLength(const std::vector<std::string>& stringArray, int sizeLimit)
+{
+   if((stringArray.size() == 0) || sizeLimit < 0)
+   {
+       cout <<"Invalid Input \n";
+       return -1;
+   }
+   displayStringInput(stringArray);
+    //cout << "The entered strings are: ";
+    //for(unsigned int i = 0; i < stringArray.size(); i++)
+    //{
+    //	cout << stringArray[i] << " ";
+    //}
+   int count = checkForSizeLimitAndPrintToConsole(stringArray,sizeLimit);
+   return count;
+
+    //int count=0;
+    //cout << "The strings that qualify are: ";
+    //for(unsigned int i = 0; i < stringArray.size(); i++)
+    //{
+    //	if(stringArray[i].length() > sizeLimit)
+    //	{
+    //	    count++;
+    //	    cout << stringArray[i] << " ";
+    //	}
+    //}
+    //return count;
 }
 
 
