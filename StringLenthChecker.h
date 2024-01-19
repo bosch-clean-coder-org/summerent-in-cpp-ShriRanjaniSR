f9f238e94407894002ae30d5f7a75729a32acb1e
ShriRanjaniSR <136423361+ShriRanjaniSR@users.noreply.github.com>
2024-01-19 11:52:58 +0530

    Update StringLenthChecker.h

--- StringLenthChecker.h
+++ StringLenthChecker.h
@@ -2,4 +2,5 @@
 #include <vector>
 using namespace std;
 
+void displayStringInput(const std::vector<std::string>& stringArray);
 int acceptStringVectorAndCheckForLength(const std::vector<std::string>& stringArray, int sizeLimit);
