360259cf3418009f34749deca083170c29e667c0
ShriRanjaniSR <136423361+ShriRanjaniSR@users.noreply.github.com>
2024-01-19 10:46:50 +0530

    Update StringLenthChecker.cpp

--- StringLenthChecker.cpp
+++ StringLenthChecker.cpp
@@ -1,45 +1,38 @@
 #include <iostream>
 
 using namespace std;
+int sizeOfArray, sizeLimit;
+std::string *collection;
 
-void acceptStringArray()
+void acceptStringCheckerInputParams()
 {
-    
-    
-}
-
-int main()
-{
-    cout<<"Hello World\n";
-
-	int x, n;
-	cout << "Enter the number of strings:" << "\n";
-	cin >> n;
-	
-	std::string *collection = new std::string[n];
-	for(unsigned int i = 0; i < n; i++)
-	{
-	    cout << "Enter string " << i+1 << endl;
+    cout << "Enter the number of strings:" << "\n";
+    cin >> sizeOfArray;
+    collection = new std::string[sizeOfArray];
+    for(unsigned int i = 0; i < sizeOfArray; i++)
+    {
+	cout << "Enter string " << i+1 << endl;
         std::cin>>collection[i];
     }
     
     cout << "The entered strings are: ";
-    for(unsigned int i = 0; i < n; i++)
-	{
-	    cout << collection[i] << " ";
+    for(unsigned int i = 0; i < sizeOfArray; i++)
+    {
+	cout << collection[i] << " ";
     }
     cout << endl;
-    
     cout << "Enter the size limit that you want to check the strings against: ";
-    cin >> x;
+    cin >> sizeLimit;
+}
 
+void checkStringsAgainstSize()
+{
     cout << "The strings that qualify are: ";
-    for(unsigned int i = 0; i < n; i++)
-	{
-	    if(collection[i].length() > x)
-	    { 
-	        cout << collection[i] << " ";
-	    }
+    for(unsigned int i = 0; i < sizeOfArray; i++)
+    {
+	if(collection[i].length() > sizeLimit)
+	{ 
+	    cout << collection[i] << " ";
+	}
     }
-    return 0;
 }
